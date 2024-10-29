bed20ae71d5e543df7a4353ae4399a070d206d08
gadiroff <53742739+gadiroff@users.noreply.github.com>
2024-10-29 10:40:08 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,38 +1,7 @@
 variable "kubernetes_groups_cluster_wide" {
   description = "Kubernetes group that will grant the cluster wide access."
   type        = any
-  default     = []
-  }
-
-variable "kubernetes_groups_namespaced" {
-  description = "Kubernetes group that will grant the namespaced wide access."
-  type        = any
-  default     = []
-}
-
-
-### Example of values for above variables #############
-kubernetes_groups_namespaced = [
-    {
-      name      = "default_namespace_admin"
-      namespace = "default"
-      role_rules = [
-        {
-          api_groups = ["*"]
-          resources  = ["*"]
-          verbs      = ["*"]
-        }
-      ]
-      subject = [
-        {
-          kind      = "Group"
-          name      = "default_namespace_admin"
-          api_group = "rbac.authorization.k8s.io"
-        }
-      ]
-    }
-  ]
-  kubernetes_groups_cluster_wide = [
+  default     = [
     {
       name = "cluster_admin"
       role_rules = [
@@ -52,4 +21,29 @@ kubernetes_groups_namespaced = [
       ]
     }
   ]
-###################################################
+}
+
+variable "kubernetes_groups_namespaced" {
+  description = "Kubernetes group that will grant the namespaced wide access."
+  type        = any
+  default     = [
+    {
+      name      = "default_namespace_admin"
+      namespace = "default"
+      role_rules = [
+        {
+          api_groups = ["*"]
+          resources  = ["*"]
+          verbs      = ["*"]
+        }
+      ]
+      subject = [
+        {
+          kind      = "Group"
+          name      = "default_namespace_admin"
+          api_group = "rbac.authorization.k8s.io"
+        }
+      ]
+    }
+  ]
+}
